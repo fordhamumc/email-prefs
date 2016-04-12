27124ef9861f4bdf94fb09dca9e8524e8d2b2792
Michael Foley <michaeldfoley@gmail.com>
2016-04-12 16:20:58 -0400

    toggleMore now accepts only the target element

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-function toggleMore() {
-    var parent = this.parentElement;
-    if (parent.classList.contains("is-open")) {
-        return parent.classList.remove("is-open")
+function toggleMore(target) {
+    if (target.classList.contains("is-open")) {
+        return target.classList.remove("is-open")
     }
-    this.parentElement.classList.add("is-open");
+    target.classList.add("is-open");
 }
 
 (function () {
     var more = document.getElementsByClassName("trigger-more");
     Array.prototype.map.call(more, function (link) {
-        link.addEventListener("click", toggleMore);
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            toggleMore(e.target.parentElement);
+        });
     });
 })();
