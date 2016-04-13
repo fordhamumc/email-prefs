cc5fff34646884a1ffc813311770be01a849986e
Michael Foley <michaeldfoley@gmail.com>
2016-04-13 12:32:44 -0400

    added select all/none options

--- js/main.js
+++ js/main.js
@@ -2,28 +2,61 @@ function toggleMore(target) {
     if (target.classList.contains("is-open")) {
         return target.classList.remove("is-open")
     }
-    target.classList.add("is-open");
+    return target.classList.add("is-open");
+}
+
+function toggleCheckboxes(type, list) {
+    inputs = list.querySelectorAll('input');
+    Array.prototype.map.call(inputs, function(input) {
+        input.checked = type === 'all';
+    });
+}
+
+function addMoreLink(list) {
+    var el = document.createElement("a");
+    el.href = "#";
+    el.className = "trigger-more pref-more";
+    el.innerText = "+ More";
+
+    el.addEventListener("click", function(e) {
+        e.preventDefault();
+        toggleMore(e.target.parentElement);
+    });
+    list.appendChild(el);
+    list.classList.add("has-more");
+}
+
+function addMultiSelects(label, list) {
+    var container = document.createElement("div");
+    container.className = "pref-multiselect";
+
+    ["all", "none"].map(function(type) {
+        var el = document.createElement("a");
+        el.href = "#";
+        el.innerText = type;
+
+        el.addEventListener("click", function(e) {
+            e.preventDefault();
+            toggleCheckboxes(type, list);
+        });
+
+        container.appendChild(el);
+    });
+    label.appendChild(container);
 }
 
 (function () {
     document.documentElement.className = "js";
 
-    var prefLists = document.getElementsByClassName("pref-list--container");
-    Array.prototype.map.call(prefLists, function (list) {
+    var prefSection = document.getElementsByClassName("pref-section");
+    Array.prototype.map.call(prefSection, function (section) {
+        var label = section.getElementsByClassName("pref-label--container")[0];
+        var list = section.getElementsByClassName("pref-list--container")[0];
+        if (label) {
+            addMultiSelects(label, list);
+        }
         if (list.firstElementChild.childElementCount > 8) {
-
-            var el = document.createElement("a");
-                el.href = "#";
-                el.className = "trigger-more pref-more";
-                el.innerText = "+ More";
-
-            el.addEventListener("click", function(e) {
-                e.preventDefault();
-                toggleMore(e.target.parentElement);
-            });
-
-            list.appendChild(el);
-            list.classList.add("has-more");
+            addMoreLink(list);
         }
     });
 })();
