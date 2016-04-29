ccf1326fba659d4a2d8b8c4b1ddb58c797b3dccb
Michael Foley <michaeldfoley@gmail.com>
2016-04-29 14:54:28 -0400

    removed v2 option

--- js/main.js
+++ js/main.js
@@ -42,66 +42,17 @@ function addMultiSelects(label, list) {
     label.appendChild(container);
 }
 
-function addCheckedCount(label, list) {
-    var el = document.createElement("div");
-        el.className = "pref-count";
-
-    updateCheckedCount(el, list);
-    label.appendChild(el);
-}
-
-function updateCheckedCount(countElement, list) {
-    var count = list.querySelectorAll('input:checked').length;
-    countElement.innerText = count + " selected";
-}
-
-function hideAllSections(prefSection) {
-    Array.prototype.map.call(prefSection, function(section) {
-        section.classList.remove("pref-section--active");
-        section.classList.add("pref-section--hidden");
-    });
-}
-
-function setActiveSection(section) {
-    section.classList.add("pref-section--active");
-    section.classList.remove("pref-section--hidden");
-}
-
-function toggleSection(section, prefSection, list) {
-    hideAllSections(prefSection);
-    setActiveSection(section);
-    updateCheckedCount(section.getElementsByClassName("pref-count")[0], list);
-    window.location.hash = section.getElementsByClassName("pref-label")[0].innerText.replace(/[^\w]/g, '').toLowerCase();
-}
-
-function setupTabs(section, prefSection, list) {
-    section.classList.add("pref-section--hidden");
-    section.addEventListener("click", function() {
-        toggleSection(section, prefSection, list);
-    });
-}
-
 (function () {
     document.documentElement.className = "js";
-    var hash = window.location.hash.replace("#","");
     var prefSection = document.getElementsByClassName("pref-section");
     Array.prototype.map.call(prefSection, function(section) {
-        var isTabs = section.parentElement.classList.contains("pref-container-tabs");
         var label = section.getElementsByClassName("pref-label--container")[0];
         var list = section.getElementsByClassName("pref-list--container")[0];
         if (label) {
             addMultiSelects(label, list);
-            addCheckedCount(label, list);
         }
-        if (list.firstElementChild.childElementCount > 8 && !isTabs) {
+        if (list.firstElementChild.childElementCount > 8) {
             addMoreLink(list);
         }
-        if (isTabs) {
-            setupTabs(section, prefSection, list);
-        }
     });
-
-    if (hash) {
-        setActiveSection(document.getElementById(hash));
-    }
 })();
