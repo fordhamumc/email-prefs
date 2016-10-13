e8e845f5c12c669aac14b2b7b51653bac5acf623
Michael Foley <michaeldfoley@gmail.com>
2016-10-13 17:06:17 -0400

    updated form to use IMC prod instance. added qa flag to switch back to qa instance

--- app/inc/header.php
+++ app/inc/header.php
@@ -3,6 +3,7 @@
 /**
  * Defaults for all pages
  *
+ * @param string $qa            Setting that describes whether to use IMC QA or IMC Prod
  * @param array  $options       An associative array preference options
  * @param array  $credentials   An associative array containing the connection parameters for IMC
  **/
@@ -10,9 +11,14 @@
 session_start();
 date_default_timezone_set('America/New_York');
 define("IMC_DIR", __DIR__."/../imc_connector");
+
+$qa = filter_input( INPUT_GET, "qa", FILTER_SANITIZE_NUMBER_INT );
+if ($qa !== null) $_SESSION["qa"] = $qa;
+
 require_once IMC_DIR."/ImcConnector.php";
 $options = json_decode(file_get_contents(IMC_DIR."/prefOptions.json"), TRUE);
-$credentials = parse_ini_file(IMC_DIR."/authData.ini", true);
+
+$credentials = parse_ini_file(IMC_DIR . (($_SESSION["qa"] == 1) ? "/authData-qa.ini" : "/authData.ini"), true);
 
 ImcConnector::getInstance($credentials["imc"]["baseUrl"]);
 ImcConnector::getInstance()->authenticateRest(
@@ -20,6 +26,18 @@ ImcConnector::getInstance()->authenticateRest(
     $credentials["imc"]["client_secret"],
     $credentials["imc"]["refresh_token"]
 );
+
+
+
+/**
+ * Remove qa flag from query string
+ *
+ * @param string $query         String of query parameters (minus qa parameter)
+ **/
+
+$query = $_GET;
+unset($query["qa"]);
+$query = http_build_query($query,'','&');
 
 ?>
 <!doctype html>
@@ -36,6 +54,9 @@ ImcConnector::getInstance()->authenticateRest(
     <link rel="stylesheet" href="css/main.css" type="text/css">
 </head>
 <body>
+<?php if ($_SESSION["qa"]) { ?>
+<div class="alert"><a href="/?<?php echo $query;?>&qa=0">Currently using the IMC QA database. <span>Switch to IMC Prod.</span></a></div>
+<?php } ?>
 <div class="container logo">
     <a href="http://fordham.edu">
         <img src="img/fordham.png" width="165" alt="Fordham University">
