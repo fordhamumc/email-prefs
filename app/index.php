d4c00aaa1e07599d2584cff7246ec2ed5f8f8c4a
Michael Foley <michaeldfoley@gmail.com>
2018-05-22 11:17:29 -0400

    Removed testing flag

--- app/index.php
+++ app/index.php
@@ -20,14 +20,10 @@ $user = new User($credentials, $options, $recipientId, $encodedId, $emailInput, 
   <header class="intro container">
     <?php if ($source === 'mcsignup'): ?>
       <h1 class="intro-heading">Subscription Confirmed</h1>
-      <div>Use the form below to customize the types of emails you receive.</div>
-    <?php elseif ($test): ?>
-      <h1 class="intro-heading">Set Your Email Preferences</h1>
-      <div>Fordham University will use the information you provide on this form to stay in touch with you. Please use the options below to customize the types of emails you receive from Fordham.</div>
     <?php else: ?>
       <h1 class="intro-heading">Set Your Email Preferences</h1>
-      <div>Check the types of emails you are interested in receiving.</div>
     <?php endif ?>
+    <div>Fordham University will use the information you provide on this form to stay in touch with you. Please use the options below to customize the types of emails you receive from Fordham.</div>
   </header>
 <?php endif ?>
 <?php if ($user->exists()): ?>
@@ -46,12 +42,10 @@ $user = new User($credentials, $options, $recipientId, $encodedId, $emailInput, 
     <footer class="form-footer">
       <input type="submit" value="Update Your Preferences" class="btn">
 
-      <?php if ($test): ?>
-        <div class="gdpr">
-          <p>You can change your mind at any time by using the unsubscribe link in the footer of all the emails you receive from us, or by sending an email to <a href="mailto:emailmarketing@fordham.edu">emailmarketing@fordham.edu</a>. You can also review our <a href="https://www.fordham.edu/info/21366/policies/8331/privacy_policy">privacy policy</a>. By submitting this form, you agree that we may process your information in accordance with these terms.</p>
-          <p>We use MailChimp and IBM Watson Campaign Automation as our marketing automation platforms. By submitting this form, you acknowledge that the information you provide will be transferred to MailChimp for processing in accordance with their <a href="https://mailchimp.com/legal/privacy/">Privacy Policy</a> and <a href="https://mailchimp.com/legal/terms/">Terms</a> and also to IBM Watson Campaign Automation in accordance with their <a href="https://www.ibm.com/privacy/us/en/">Privacy Policy</a> and <a href="https://www.ibm.com/legal/us/en/">Terms</a>.</p>
-        </div>
-      <?php endif ?>
+      <div class="gdpr">
+        <p>You can change your mind at any time by using the unsubscribe link in the footer of all the emails you receive from us, or by sending an email to <a href="mailto:emailmarketing@fordham.edu">emailmarketing@fordham.edu</a>. You can also review our <a href="https://www.fordham.edu/info/21366/policies/8331/privacy_policy">privacy policy</a>. By submitting this form, you agree that we may process your information in accordance with these terms.</p>
+        <p>We use MailChimp and IBM Watson Campaign Automation as our marketing automation platforms. By submitting this form, you acknowledge that the information you provide will be transferred to MailChimp for processing in accordance with their <a href="https://mailchimp.com/legal/privacy/">privacy policy</a> and <a href="https://mailchimp.com/legal/terms/">terms</a> and also to IBM Watson Campaign Automation in accordance with their <a href="https://www.ibm.com/privacy/us/en/">privacy policy</a> and <a href="https://www.ibm.com/legal/us/en/">terms</a>.</p>
+      </div>
     </footer>
     <input type="hidden" name="formSourceName" value="StandardForm">
     <!-- DO NOT REMOVE HIDDEN FIELD sp_exp -->
